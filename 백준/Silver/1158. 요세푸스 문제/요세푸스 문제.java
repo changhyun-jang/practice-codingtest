8d4d73ea93a408f89a6309b9678aded64ac2bfbb
changhyun-jang <86968048+changhyun-jang@users.noreply.github.com>
2023-07-12 17:44:45 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 828 ms, Memory: 296884 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("<");
+        Deque<Integer> queue=new LinkedList<>();
+        String command=br.readLine();
+        int count=1;
+        int save;
+        String []command2=command.split(" ");
+        for(int i=1; i<Integer.parseInt(command2[0])+1;i++){
+            queue.add(i);
+        }
+        while(!queue.isEmpty()){
+            if(queue.size()==1){
+                bw.write(String.valueOf(queue.pop()));
+                break;
+            }
+            save=queue.pop();
+            if(count==Integer.parseInt(command2[1])){
+                bw.write(String.valueOf(save)+", ");
+                count=1;
+
+            }else{
+                queue.add(save);
+                count++;
+            }
+        }
+        bw.write(">");
+        bw.flush();
+        bw.close();
+    }
+}
