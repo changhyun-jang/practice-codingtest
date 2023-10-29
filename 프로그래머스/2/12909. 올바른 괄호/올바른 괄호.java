a3191296141b94e319d67dab946e35f6779212da
changhyun-jang <86968048+changhyun-jang@users.noreply.github.com>
2023-10-29 22:30:21 +0900

    [level 2] Title: 올바른 괄호, Time: 6.05 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,14 +1,28 @@
 class Solution {
     boolean solution(String s) {
-       boolean answer = true;
-        int count =0;
-       for(char c:s.toCharArray()){
-           if(count<0) return false;
-            if(c=='(') count+=1;
-            else count-=1;
+        boolean answer = true;
+        
+        int left = 0;
+        int right = 0;
+        
+       for(char c : s.toCharArray()){
+           if(c=='(') left++;
+           else if(c==')')right++;
+           if(left<right){
+               answer = false;
+               return answer;
+           }
+       } 
+        
+        if(left==right) {
+            if(s.charAt(0)==')'){
+                answer=false;
+                return answer;
+            }
+            return answer;
+        }else {
+            answer = false;
+            return answer;
         }
-       if(count!=0) return false;
-
-        return answer;
     }
 }
