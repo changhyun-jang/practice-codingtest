5ab656fe14ca287e7a363566bce0576d4454315e
changhyun-jang <86968048+changhyun-jang@users.noreply.github.com>
2024-05-24 00:13:57 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,50 +1,58 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int total = Integer.parseInt(br.readLine()); //스위치의 개수
-		int[] switches = new int[total];
-		StringTokenizer st = new StringTokenizer(br.readLine()); //스위치 string 받음
-		for(int i=0; i<total; i++)
-			switches[i] = Integer.parseInt(st.nextToken());
-		
-		int stuCnt = Integer.parseInt(br.readLine()); //학생의 명수
-		for(int i=0; i<stuCnt; i++) {
-			st = new StringTokenizer(br.readLine());
-			int gender = Integer.parseInt(st.nextToken());
-			int number = Integer.parseInt(st.nextToken());
-			
-			//남학생이면
-			if(gender == 1) {
-				for(int j=0; j<total; j++) //뽑은 수의 배수 위치에 있는 스위치의 상태를 바꾼다.
-					if((j+1) % number == 0)
-						switches[j] = switches[j] == 0? 1: 0;
-			}
-			//여학생이면
-			else {
-				//뽑은 수를 중심으로 좌우가 대칭이면 상태를 바꾼다.
-				switches[number - 1] = switches[number - 1] == 0 ? 1 : 0;
-				for(int j=1; j<total/2; j++) {
-					if(number - 1 + j >= total || number - 1 - j < 0)
-						break;
-					if(switches[number - 1 - j] == switches[number - 1 + j]) {
-						switches[number - 1 - j] = switches[number - 1 - j] == 0 ? 1 : 0;
-						switches[number - 1 + j] = switches[number - 1 + j] == 0 ? 1 : 0;
-					}
-					else break; //대칭 아닌것이 나오면 바로 끝낸다.
-				}
-			}
-		}
-		
-		//한 줄에 20개씩 출력
-		for(int i=0; i<total; i++) {
-			System.out.print(switches[i] + " ");
-			if((i+1) % 20 == 0)
-				System.out.println();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        boolean[] S = new boolean[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            if (temp == 0) {
+                S[i] = false;
+            } else {
+                S[i] = true;
+            }
+        }
+        st = new StringTokenizer(br.readLine());
+        int P = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < P; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            if (num == 2) {
+                S[M] = !S[M];
+                int L = M - 1;
+                int R = M + 1;
+                while (L > 0 && R <= N) {
+                    if (S[L] != S[R]) {
+                        break;
+                    } else {
+                        S[L] = !S[L];
+                        S[R] = !S[R];
+                        L--;
+                        R++;
+                    }
+                }
+            } else {
+                for (int j = M; j <= N; j += M) {
+                    S[j] = !S[j];
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (count == 20) {
+                System.out.println();
+                count=0;
+            }
+            if (S[i]) {
+                System.out.print(1 + " ");
+            } else {
+                System.out.print(0 + " ");
+            }
+            count++;
+        }
+    }
 }
