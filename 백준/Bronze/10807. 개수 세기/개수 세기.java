69d7c5c4f0216e137044f10e03b4d5617b67a756
changhyun-jang <86968048+changhyun-jang@users.noreply.github.com>
2024-07-12 14:26:48 +0900

    [Bronze V] Title: 개수 세기, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            if (map.containsKey(tmp)) {
+                map.put(tmp, map.get(tmp) + 1);
+            } else {
+                map.put(tmp, 1);
+            }
+        }
+        int answer = Integer.parseInt(br.readLine());
+        if (map.get(answer) == null) {
+            System.out.println(0);
+        } else {
+            System.out.println(map.get(answer));
+        }
+    }
+}
