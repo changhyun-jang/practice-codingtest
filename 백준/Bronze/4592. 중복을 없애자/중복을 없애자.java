ee38b3a89e2a06911806d1c2e36e439410dde623
changhyun-jang <86968048+changhyun-jang@users.noreply.github.com>
2024-07-13 19:35:58 +0900

    [Bronze II] Title: 중복을 없애자, Time: 112 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Bronze/4592. 중복을 없애자/중복을 없애자.java
+++ 백준/Bronze/4592. 중복을 없애자/중복을 없애자.java
@@ -4,25 +4,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
         StringTokenizer st;
         while (true) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             if (N == 0) break;
-            ArrayList<Integer> arr = new ArrayList<>();
-            arr.add(Integer.parseInt(st.nextToken()));
-            for (int test_case = 0; test_case < N-1; test_case++) {
-                int temp = arr.size();
+            int now = Integer.parseInt(st.nextToken());
+            sb.append(now + " ");
+            for (int test_case = 0; test_case < N - 1; test_case++) {
                 int num = Integer.parseInt(st.nextToken());
-                if (arr.get(temp - 1) != num) {
-                    arr.add(num);
+                if (now != num) {
+                    sb.append(num + " ");
+                    now = num;
                 }
             }
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.print("$");
-            System.out.println();
+            sb.append("$\n");
         }
+        System.out.print(sb);
     }
 }
