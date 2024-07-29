0ac82e94cbfee4ed542d268a42e6106be28b527c
장창현 <86968048+changhyun-jang@users.noreply.github.com>
2024-07-29 21:56:25 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -0,0 +1,6 @@
+select e1.id, e1.genotype, e2.genotype as parent_genotype
+from ecoli_data e1
+join ecoli_data e2
+on e1.parent_id = e2.id
+where e1.genotype & e2.genotype = e2.genotype
+order by e1.id;
